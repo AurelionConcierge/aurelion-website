9a0ae844f571b1fed862c62e2941f9f52fc547aa
AurelionConcierge <care@aurelionconcierge.com>
2026-04-25 16:32:42 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import ChatWidget from '../components/ChatWidget'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <ChatWidget />
+    </>
+  )
 }
 
 export default MyApp
