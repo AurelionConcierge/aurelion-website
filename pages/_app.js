4ad3fdb0e332fe54d2ed61295efd4adeed6d6c6f
AurelionConcierge <care@aurelionconcierge.com>
2026-05-12 11:22:12 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import ChatWidget from '../components/ChatWidget'
+import WhatsAppButton from '../components/WhatsAppButton'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <ChatWidget />
+      <WhatsAppButton />
     </>
   )
 }
